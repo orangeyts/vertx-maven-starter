0a28f710349177b6422655e306c3f568a8debbc4
orangehs <1191066931@qq.com>
2018-05-14 12:05:19 +0800

    print machine Id from server

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -7,8 +7,9 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public void start() {
     System.out.println(String.format("deploy port %s",config().getInteger("port")));
+    System.out.println(String.format("machineId %s",config().getInteger("mid")));
     vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
+        .requestHandler(req -> req.response().end("Hello Vert.x! from " + config().getInteger("mid")))
         .listen(config().getInteger("port",8888));
   }
 
