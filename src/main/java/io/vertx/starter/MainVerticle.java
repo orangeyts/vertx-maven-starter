7b6b0a94d326b596937d3f714c69cde9ab976ce3
orangehs <1191066931@qq.com>
2018-05-14 11:51:12 +0800

    readMe-Run the project

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -6,7 +6,7 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
-    System.out.println(String.format("deploy port %s",config().getString("port")));
+    System.out.println(String.format("deploy port %s",config().getInteger("port")));
     vertx.createHttpServer()
         .requestHandler(req -> req.response().end("Hello Vert.x!"))
         .listen(config().getInteger("port",8888));
